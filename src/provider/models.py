82ff0f5040f64d2a256160785d9ea5f131251bca
GilbertWil <gramosti@unsa.edu.pe>
2020-07-11 16:18:54 -0500

    hice el modelo que se usara para los proveedores

--- src/provider/models.py
+++ src/provider/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+	ruc = models.IntegerField()
+	razonSocial = models.CharField(max_length = 100)
+	nombre = models.CharField()
+	direccion = models.CharField()
+	celular = models.IntegerField()
+	ciudad = models.CharField()
