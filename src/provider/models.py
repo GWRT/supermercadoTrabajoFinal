7a2673a21973f17aa0fe6cafcd9021d3d2b94666
GilbertWil <gramosti@unsa.edu.pe>
2020-07-11 20:46:19 -0500

    ahora no me da error. Cree el modelo para los proveedores

--- src/provider/models.py
+++ src/provider/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class provider(models.Model):
+	ruc = models.IntegerField()
+	razonSocial = models.CharField(max_length = 100)
+	persona = models.CharField(max_length = 100)
+	direccion = models.CharField(max_length = 200)
+	email = models.EmailField(max_length = 254)
+	telefono = models.IntegerField()
+	estado =  models.BooleanField(default = False)
