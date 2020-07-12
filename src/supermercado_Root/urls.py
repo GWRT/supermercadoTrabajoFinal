895fb8ba7b841617d8b6a844fd101fc60eb44700
GilbertWil <gramosti@unsa.edu.pe>
2020-07-11 21:46:43 -0500

    referencio mi vista a una url

--- src/supermercado_Root/urls.py
+++ src/supermercado_Root/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from provider.views import provider
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('provider/' provider, name='proveedores'),
 ]
