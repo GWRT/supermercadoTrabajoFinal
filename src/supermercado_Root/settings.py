e51c15111f97c2d2a37072ef26136c845f43d768
GilbertWil <gramosti@unsa.edu.pe>
2020-07-11 20:18:17 -0500

    cree una aplicacion, haber si esta vez no me da error

--- src/supermercado_Root/settings.py
+++ src/supermercado_Root/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Aqui iran mis aplicaciones
+
+    'indexPagina',
+    'provider',
 ]
 
 MIDDLEWARE = [
