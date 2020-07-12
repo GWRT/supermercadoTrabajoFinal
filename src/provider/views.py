1477ed978d8e1ec465be76aa7d2fb2ed0e9b3426
GilbertWil <gramosti@unsa.edu.pe>
2020-07-11 21:39:14 -0500

    agrege la vista para el model

--- src/provider/views.py
+++ src/provider/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import provider
 
 # Create your views here.
+def provider (request):
+	prov = provider.objects.all()
+
+	return render(request, 'provider/proveedores.html',{'prov':prov})
