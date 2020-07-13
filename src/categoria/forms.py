0fea1b1efcd46ff36f9a932bbdffc2cfc0ce504a
GilbertWil <gramosti@unsa.edu.pe>
2020-07-12 23:02:06 -0500

    agrege codigo para que funcione la vista modificar y eliminar

--- src/categoria/forms.py
+++ src/categoria/forms.py
@@ -14,6 +14,6 @@ class categoryForm(forms.ModelForm):
 		}
 
 	def __init__(self, *args, **kwargs):
-		super(providerForm,self).__init__(*args,**kwargs)
+		super(categoryForm,self).__init__(*args,**kwargs)
 		self.fields['name'].widget.attrs.update({'class':'mdl-textfield__input'})
 		self.fields['desc'].widget.attrs.update({'class':'mdl-textfield__input'})	
--- src/categoria/views.py
+++ src/categoria/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category
+from .forms import categoryForm
 
 # Create your views here.
 def categories(request):
@@ -8,10 +9,35 @@ def categories(request):
 	return render(request, 'categoria/categorias.html', {'cats':cats})
 
 def crearCategoria(request):
-	return render(request, 'categoria/categorias.html', {})	
+	context = {
+		'form' : categoryForm()
+	}
+
+	if request.method == 'POST':
+		formulario = categoryForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			print('guardado')
+			return redirect(to = 'category')
+
+		context['form'] = formulario
+
+	return render(request, 'categoria/create.html', context)	
 
 def modificarCategoria(request, id):
-	return render(request, 'categoria/categorias.html', {})	
+	cats = Category.objects.get(id = id)
+
+	if request.method == 'POST':
+		form = 	categoryForm(data=request.POST, instance=cats)
+
+		if form.is_valid():
+			form.save()
+			return redirect('category')
+
+	return render(request, 'categoria/modificar.html',{'form':categoryForm(instance=cats)})
 
 def eliminarCategoria(request, id):
-	return render(request, 'categoria/categorias.html', {})			
+	cats = Category.objects.get(id = id)
+	cats.delete()
+
+	return redirect(to='category')			
