beca4888701dd194853459aa33f5fda3c6cf227a
GilbertWil <gramosti@unsa.edu.pe>
2020-07-12 22:44:30 -0500

    agrege el formulario de categorias

---
+++ src/categoria/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Category
+
+class categoryForm(forms.ModelForm):
+	class Meta:
+		model = Category
+		fields = [
+			'name',
+			'desc',
+		]
+		labels = {
+			'name' : 'Nombre',
+			'desc' : 'Descripcion',
+		}
+
+	def __init__(self, *args, **kwargs):
+		super(providerForm,self).__init__(*args,**kwargs)
+		self.fields['name'].widget.attrs.update({'class':'mdl-textfield__input'})
+		self.fields['desc'].widget.attrs.update({'class':'mdl-textfield__input'})	
