bc9d44ffb00ed2f15635e7ad2e1e7c65a4e02501
GilbertWil <gramosti@unsa.edu.pe>
2020-07-12 21:55:06 -0500

    agregue unsa vista para listar

--- src/categoria/views.py
+++ src/categoria/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
+def categories(request):
+	cats = Category.objects.all()
+
+	return render(request, 'categoria/categorias.html', {'cats':cats})
