2209fdd6c1fbb47640622804e4e387c6253d7e62
jchile <jchile@unsa.edu.pe>
2020-07-13 21:02:03 -0500

    creacion del model product

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	barcode = models.IntegerField()
+	name = models.CharField(max_length=20)
+	units = models.IntegerField()
+	price = models.IntegerField()
+	img = models.ImageField(upload_to='pics')
