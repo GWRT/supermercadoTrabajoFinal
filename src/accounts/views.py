7e9a86ef590f573d15ee73587adc7150c48ea2a8
jchile <jchile@unsa.edu.pe>
2020-07-29 16:37:03 -0500

    view de register

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
+
+def signup(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return redirect('/')
+		else:
+			form = SignUpForm()
+		return render(request, 'signup.html',{'form':form})
+
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
