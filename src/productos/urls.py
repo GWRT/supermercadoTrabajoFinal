f524058aafa640cd1fd053fde0cd95088e3b346e
GilbertWil <gramosti@unsa.edu.pe>
2020-08-12 21:36:36 -0500

    css lista productos

--- src/productos/urls.py
+++ src/productos/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('addProduct/',views.addProduct.as_view(),name='addProduct'),
     path('deleteProduct/<int:pk>/',views.deleteProduct.as_view(),name='deleteProduct'),
     path('updateProduct/<int:pk>/',views.updateProduct.as_view(),name='updateProduct'),
-    path('listProductPDF/',views.listProductPDF,name='listProductPDF'),
+    path('listProductPDF/',views.listProductPDF.as_view(),name='listProductPDF'),
 
 ]
